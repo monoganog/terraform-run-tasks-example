987e047de8891fe1618c079f5da617d3d9ffcfad
Ben Monaghan <monoganog@gmail.com>
2024-08-05 09:18:50 +0100

    Basic VM

---
+++ main.tf
@@ -0,0 +1,48 @@
+terraform {
+
+  cloud {
+    organization = "ben-monaghan-org"
+
+    workspaces {
+      name = "run-tasks-example"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.31.0"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+}
